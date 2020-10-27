054dbbe773c40c269614a3e4a6adbdd9303f1108
KateShevchenk0 <73313406+KateShevchenk0@users.noreply.github.com>
2020-10-27 22:38:43 +0200

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <meta charset = "utf-8">
+  <meta name = "author" content = "Kate Shevchenko">
+    <title> 🌸 Зв'язок зі мною </title>
+      <body style="background: #00FFFF">
+        <center>
+        <hr size="6" noshade>
+          <p><a href="https://kateshevchenk0.github.io/cv/PersonalSite.html">Про мене</a></p>
+        <table>
+    <tr>
+      <td></td>
+    </tr>
+  </table>
+<form class="" action="mailto:6171981@stud.nau.edu.ua" method="post" enctype="text/plain">
+    <hr>
+      <label for="">Ваше ім'я:</label>
+      <input type="text" name="yourName" value=""><br>
+      <label for="">Ваш Email:</label>
+      <input type="email" name="yourEmail" value="" ><br>
+      <label for="">Ваше повідoмлення:</label><br>
+      <textarea name="name" rows="10" cols="30"></textarea><br>
+      <input type="submit">
+    </form>
+    </form>
+    </center>
+      <h4><a href = "Контакти.html">Назад</a></h4>
+<hr size="6" noshade>
+</body>
+</head>
+</html>
